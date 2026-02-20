6bc928238c2550009874a812f12c839ab1d77705
hmabed <hmabed@gmail.com>
2026-02-20 10:58:04 +0100

    Change PocketBase URL to local development server

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -2,5 +2,5 @@ import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
 
 
-const pb = new PocketBase('https://jbassil-agence.pockethost.io/') as TypedPocketBase;
+const pb = new PocketBase('http://localhost:8090/') as TypedPocketBase;
 export default pb;
